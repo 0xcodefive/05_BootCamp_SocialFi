ef8ff04fced657ed9983e242fbf2cea4c97c7c05
VAK <vak@pina.su>
2023-04-13 22:52:17 +0500

    modified:   handlers.js

--- handlers.js
+++ handlers.js
@@ -12,6 +12,7 @@ console.error(`Address signer: ${addressSigner}`);
 const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer);
 const WAIT_BLOCK_CONFIRMATIONS = 2;
 
+// Функция минта токенов. Автоматически проверяется минимальное требуемое количество для минта
 async function safeMint(address) {
   const price = await contract.priceToMint(address);
   const tx = await contract.safeMint({ value: price });
@@ -19,22 +20,21 @@ async function safeMint(address) {
   await signer.provider.waitForTransaction(tx.hash, WAIT_BLOCK_CONFIRMATIONS);
 }
 
-async function donateEth(author, valueFromEther) {
-  const tx = await contract.donateEth(author, {
-    value: ethers.utils.parseEther(valueFromEther),
-  });
+// Донат в Коине блокчейна ВНИМАНИЕ, функция принимает значение кратное 10**18, то есть единице Ether
+async function donateEthByEther(author, valueFromEther) {
+  const value = ethers.utils.parseEther(valueFromEther.toString());
+  const tx = await contract.donateEth(author, { value: value });
   console.log(`donate hash: ${tx.hash}`);
 }
 
-async function donateToken(tokenAddress, tokenAmountFromEther, author) {
-  const tx = await contract.donateToken(
-    tokenAddress,
-    ethers.utils.parseEther(tokenAmountFromEther),
-    author
-  );
+// Донат в Токенах ВНИМАНИЕ, функция принимает значение кратное 10**18, то есть единице Ether
+async function donateTokenByEther(tokenAddress, tokenAmountFromEther, author) {
+  const value = ethers.utils.parseEther(tokenAmountFromEther.toString());
+  const tx = await contract.donateToken(tokenAddress, value, author);
   console.log(`donateToken hash: ${tx.hash}`);
 }
 
+// Получение токенов пользователя, возвращает количество токенов и их id
 async function getUsersTokens(address) {
   const balance = await contract.balanceOf(address);
   let tokens = [];
@@ -68,6 +68,8 @@ async function main() {
   console.error(
     `User token scnd time balance: ${usersToken.balance}, tokens: ${usersToken.tokens}`
   );
+
+  await donateEthByEther(usersToken.tokens[0], 0.0282828);
 }
 
 main().catch((error) => {
